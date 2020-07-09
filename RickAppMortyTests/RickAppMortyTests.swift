a313a2f17f3484c15eb9f924236f3282ff2ccdca
Gabriel Palhares <gaapalhares@gmail.com>
2020-07-09 16:05:25 -0300

    using scan for automated Unit Tests

--- RickAppMortyTests/RickAppMortyTests.swift
+++ RickAppMortyTests/RickAppMortyTests.swift
@@ -11,24 +11,21 @@ import XCTest
 
 class RickAppMortyTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testFakeSuccess() {
+        XCTAssert(true)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testFakeFailure() {
+        XCTAssert(true)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    override func tearDown() {
+        super.tearDown()
     }
 
 }
